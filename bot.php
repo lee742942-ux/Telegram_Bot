0999e8d53cef53a45feddd585fb1653941581ede
lee742942-ux <lee742942@gmail.com>
2026-05-05 14:16:45 +0200

    Replace newline character with PHP_EOL in debug logging

--- bot.php
+++ bot.php
@@ -1,9 +1,9 @@
 <?php
 
-file_put_contents("debug.txt", "HIT: " . date("H:i:s") . "\n", FILE_APPEND);
+file_put_contents("debug.txt", "HIT: " . date("H:i:s") . PHP_EOL, FILE_APPEND);
 
 $input = file_get_contents("php://input");
-file_put_contents("debug.txt", "INPUT: " . $input . "\n", FILE_APPEND);
+file_put_contents("debug.txt", "INPUT: " . $input . PHP_EOL, FILE_APPEND);
 
 http_response_code(200);
 
