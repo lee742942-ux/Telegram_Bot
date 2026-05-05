6932ff83cacc323f7e87b6d8fc3cbb9dcad8730d
lee742942-ux <lee742942@gmail.com>
2026-05-05 13:39:24 +0200

    Update bot token in bot.php

--- bot.php
+++ bot.php
@@ -2,7 +2,7 @@
 
 http_response_code(200);
 
-$botToken = "8651227813:AAEJVAM6IDkPQ0s4Q7I5eMz8g4nR6eci0zU";
+$botToken = "8651227813:AAHkYD2pFFS6uTogvpnLmCwry8cCLSxCKWg";
 $website = "https://api.telegram.org/bot".$botToken;
 
 $update = json_decode(file_get_contents("php://input"), TRUE);
