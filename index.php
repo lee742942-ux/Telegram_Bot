82b9f43b3a15230a1735de766e2b8a08abb0322f
lee742942-ux <lee742942@gmail.com>
2026-05-06 13:32:43 +0200

    Rename log file and update response message

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-file_put_contents("test.log", "HIT\n", FILE_APPEND);
+file_put_contents("hit.log", "HIT\n", FILE_APPEND);
 
 header("Content-Type: text/plain");
-echo "BOT IS RUNNING";
+echo "OK";
